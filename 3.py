4be6cbf5b04f074024539dec6872fdb8d075d552
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2019-02-16 14:12:14 +0800

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+import pandas
+
+def duplicate(nums):
+    for i in range(len(nums)):
+        print(nums[i])
+
+
+if __name__ == "__main__":
+    print("hello")
+    test = [2,3,1,0,2,5,3]
+    duplicate(test)
+    
